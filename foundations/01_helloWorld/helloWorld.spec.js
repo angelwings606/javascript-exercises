a59d0e329289308fb5877f248257e9cfa555aee1
angelwings606 <orteza@usc.edu>
2026-02-02 02:45:31 -0800

    exercise 1

--- foundations/01_helloWorld/helloWorld.spec.js
+++ foundations/01_helloWorld/helloWorld.spec.js
@@ -1,6 +1,10 @@
-const helloWorld = require('./helloWorld');
+// spec file is a plain js file used for testing 
+// which follows the principles of behavior-driven 
+// development(BDD) and defines how code should 
+// behave undedr various circumstances
+const helloWorld = require('./helloWorld'); //imports code from js file
 
-describe('Hello World', function() {
+describe('Hello World', function() { //body of test
   test('says "Hello, World!"', function() {
     expect(helloWorld()).toEqual('Hello, World!');
   });
