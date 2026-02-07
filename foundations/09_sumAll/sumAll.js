4f21935fb26a514c226bc4727a47dfc649b47e13
angelwings606 <orteza@usc.edu>
2026-02-07 07:49:56 -0800

    exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(numA, numB) {
+    if(numA < 0 || numB < 0 || !Number.isInteger(numA) || !Number.isInteger(numB)){
+        return "ERROR";
+    }
+    sum = 0
+    if(numA < numB){
+        for(i = numA; i <= numB; i++){
+            sum += i;
+        }
+    }else{
+        for(i = numB; i <= numA; i++){
+            sum += i;
+        }
+    }
+    return sum;
+    
 };
 
 // Do not edit below this line
