43d72fa8c15f9b653527e1304763b54784a78620
angelwings606 <orteza@usc.edu>
2026-02-07 07:36:58 -0800

    exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...str) {
+    newArr = [];
+    for(i=0; i< arr.length; i++){
+        if(!str.includes(arr[i])){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
