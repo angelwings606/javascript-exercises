a30045a7995bd81967d3a7092dde0c0d98bbeb5d
angelwings606 <orteza@usc.edu>
2026-02-16 19:41:49 -0800

    exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  ctemp = (fTemp - 32) * 5/9;
+  return Math.round(ctemp*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  ftemp = (cTemp * (9/5)) + 32;
+  return Math.round(ftemp * 10)/10
 };
 
 // Do not edit below this line
