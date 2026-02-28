b27bf84236179547b2a455abe26016b5c8de34d2
angelwings606 <orteza@usc.edu>
2026-02-27 22:20:27 -0800

    find the oldest in arr

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    //return person object
+    const oldest = arr.sort((a,b) => ((b.yearOfDeath || ((new Date()).getFullYear())) - b.yearOfBirth) - ((a.yearOfDeath || ((new Date()).getFullYear()))- a.yearOfBirth));
+    return oldest[0];
 };
 
 // Do not edit below this line
