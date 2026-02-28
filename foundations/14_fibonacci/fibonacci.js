4fdd09c82978a78f351cc28c8d62282d904dcf41
angelwings606 <orteza@usc.edu>
2026-02-27 22:02:18 -0800

    fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    //check that num is correct type
+    let index;
+    if(typeof num !== 'number'){
+        index = parseInt(num);
+    }else{
+        index = num
+    }
+
+    if (index < 0) return "OOPS";//because negative
+    if (index == 0) return 0;//zero check
+    //fibonacci function
+
+    let firstPrev = 1 //two variables to tracks, starts with 1 for index 1
+    let secondPrev = 0;
+
+    for(i = 2; i <= index; i++){
+        let current = firstPrev + secondPrev; //current = 1 for index 0
+        secondPrev = firstPrev; //secondPrev is the next in sequence
+        firstPrev = current; //firstPrev is current iterator
+    }
+
+    return firstPrev
+    
 
 };
 
