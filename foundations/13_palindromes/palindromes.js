1b4839a8a18b0abb9932901eb221ed4d09e79c8e
angelwings606 <orteza@usc.edu>
2026-02-27 21:04:48 -0800

    typeerror palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (arr) {
+    const tempA = arr.slice(Math.floor(arr.length/2));
+    const tempB = arr.slice(0,Math.ceil(arr.length/2));
+    const reversed = tempB.slice().reverse();
+    return tempA === reversed;
 };
 
 // Do not edit below this line
